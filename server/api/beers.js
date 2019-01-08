a05b0a9c42e390af59fb17e6afd8873b1afb30e0
Matt Spatafore <mattsnyc93@gmail.com>
2019-01-08 14:00:43 -0500

    added routes for single beer, single user, and all beers

---
+++ server/api/beers.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const {Beer} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const beers = await Beer.findAll()
+    res.send(beers)
+  } catch (err) {
+    next(err)
+  }
+})
+
+
+router.get('/:id', async (req, res, next) {
+  try {
+    const beer = await Beer.findOne({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.send(beer);
+  } catch(err) {
+    next(err)
+  }
+})
