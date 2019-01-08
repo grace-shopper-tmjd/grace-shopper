48aeb0d462901913177a3d8a7936cc7f4416fa94
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-01-08 15:47:18 -0500

    created order model & tests, added associations to index

---
+++ server/db/models/order.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const db = require('./_db');
+
+const Order = db.define('order', {
+  orderNumber: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  orderDate: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  shipped: {
+    type: Sequelize.BOOLEAN,
+    default: false,
+    allowNull: false
+  },
+  quanitity: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+
+module.exports = Order;
---
+++ server/db/models/order.spec.js
@@ -0,0 +1,34 @@
+/* global describe beforeEach it */
+
+const { expect } = require('chai')
+const db = require('../index')
+const Order = db.model('order')
+
+describe('Order model', () => {
+  describe('Validations', () => {
+    it('requires `order number`', async () => {
+      const order = Order.build();
+
+      try {
+        await order.validate()
+        throw Error('validation was successful but should have failed without `name`');
+      }
+      catch (err) {
+        expect(err.message).to.contain('order number cannot be null');
+      }
+    });
+
+    it('requires `price` to not be an integer', async () => {
+      const order = Order.build({
+        price: ''
+      });
+
+      try {
+        await order.validate()
+        throw Error('validation was successful but should have failed if price is an empty string');
+      } catch (err) {
+        expect(err.message).to.contain('Validation error');
+      }
+    });
+  });
+});
