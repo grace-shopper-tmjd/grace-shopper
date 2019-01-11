a1c8f6666d3409447cc1337aa047cc53fca7cc54
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-01-11 18:18:10 -0500

    added rows and columns to cart item

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import {Button, ListGroupItem, Image} from 'reactstrap'
+import {Container, Row, Col, Button, ListGroupItem, Image} from 'reactstrap'
 
 const CartItem = props => (
   <ListGroupItem>
-    {props.order.beer.name}
-    {props.order.beer.price}
-    {props.order.beer.quantity}
-    {props.order.beer.packSize}
-    {/* <Image src={props.order.beer.imageUrl} /> */}
-    {props.order.beer.beerStyle.name}
+    <Container>
+      <Row>
+        <Col>{props.order.beer.name}</Col>
+        <Col>{props.order.beer.price}</Col>
+      </Row>
+      <Row>
+        <Col>{props.order.beer.quantity}</Col>
+        <Col>{props.order.beer.packSize}</Col>
+      </Row>
+      <Row>
+        <Col>{props.order.beer.beerStyle.name}</Col>
+        <Col>
+          <img style={{size: '20%'}} src={props.order.beer.imageUrl} />
+        </Col>
+      </Row>
+      <Button color="primary" />
+    </Container>
   </ListGroupItem>
 )
 
