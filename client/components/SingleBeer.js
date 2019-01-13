b9d7d160c7634175d657f6a6aa69595a4a03a400
topkoong <engineer_top@msn.com>
2019-01-12 19:33:57 -0500

    AddCart feature is working

--- client/components/SingleBeer.js
+++ client/components/SingleBeer.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {Container, Row, Col, Button, Input, Label, FormGroup} from 'reactstrap'
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Input,
+  Label,
+  FormGroup,
+  Form
+} from 'reactstrap'
 
-import {fetchSingleBeer} from '../actions/index'
+import {fetchSingleBeer, addToCart} from '../actions/index'
 
 class SingleBeer extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    // this.state = {
+    //   beerId: 0,
+    //   quantity: 1,
+    //   price: 1
+    // }
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
     this.props.getSingleBeer(this.props.match.params.beerId)
+  }
+  handleSubmit() {
+    this.props.addSingleBeer({
+      beerId: this.props.beer.id,
+      quantity: 10,
+      price: this.props.beer.price * 10
+    })
   }
 
   render() {
@@ -29,25 +50,25 @@ class SingleBeer extends Component {
             <h3>{this.props.beer.name}</h3>
             <h4>{this.props.beer.brand}</h4>
             <h5>Price: {this.props.beer.price}</h5>
-            <FormGroup>
-              <Label for="exampleSelect">Pack Size</Label>
-              <Input type="select" name="select" id="exampleSelect">
-                <option>1</option>
-                <option>6 PK</option>
-                <option>12 PK</option>
-                <option>24 PK</option>
-                <option>36 PK</option>
-              </Input>
-              <Label for="exampleSelect">Quantity</Label>
-              <Input type="select" name="select" id="exampleSelect">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Input>
-            </FormGroup>
-            <Button>Add To Cart</Button>
+            <Form>
+              <FormGroup>
+                <Label for="exampleSelect">Pack Size</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                  <option>1</option>
+                  <option>4 PK</option>
+                  <option>6 PK</option>
+                </Input>
+                <Label for="exampleSelect">Quantity</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Input>
+              </FormGroup>
+            </Form>
+            <Button onClick={this.handleSubmit}>Add To Cart</Button>
             <h4>Product Description</h4>
             <p>{this.props.beer.description}</p>
           </Col>
@@ -65,7 +86,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    getSingleBeer: id => dispatch(fetchSingleBeer(id))
+    getSingleBeer: id => dispatch(fetchSingleBeer(id)),
+    addSingleBeer: id => dispatch(addToCart(id))
   }
 }
 
